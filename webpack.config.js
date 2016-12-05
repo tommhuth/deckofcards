a4837544ae6fa5684292947c75a68527932e2920
Tomm Huth <tomm.huth@hotmail.com>
2016-12-05 17:18:41 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ let plugins = [
 	new webpack.DefinePlugin({
 		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
 	})
-];
+]
 
 if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
 	plugins.push(
